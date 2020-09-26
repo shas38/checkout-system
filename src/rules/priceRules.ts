c12f8ad162027120478c1d8aed3b0395006a89e4
skhan <skhan@staff.compass.edu.au>
2020-09-26 16:38:15 +1000

    ipd price rules added

--- src/rules/priceRules.ts
+++ src/rules/priceRules.ts
@@ -3,4 +3,9 @@ export default {
     [Sku.ATV]: function(items, price){
         return (items['atv']%3 !== 0) ? price : 0;
     },
+    [Sku.IPD]: function(items, price, bulkPrice = 499.99){
+        if(items['ipd'] < 5) return price;
+        else if(items['ipd'] === 5) return bulkPrice - (price - bulkPrice) * 4;
+        else return bulkPrice;
+    },
 }
