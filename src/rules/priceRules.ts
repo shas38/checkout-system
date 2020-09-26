da5b93ad63a67ce047fc7d09b768f8a26cffb2bd
skhan <skhan@staff.compass.edu.au>
2020-09-26 16:51:01 +1000

    vga price rules added

--- src/rules/priceRules.ts
+++ src/rules/priceRules.ts
@@ -1,11 +1,15 @@
 import Sku from '../enums/sku'
+
 export default {
     [Sku.ATV]: function(items, price){
-        return (items['atv']%3 !== 0) ? price : 0;
+        return (items[Sku.ATV]%3 !== 0) ? price : 0;
     },
     [Sku.IPD]: function(items, price, bulkPrice = 499.99){
-        if(items['ipd'] < 5) return price;
-        else if(items['ipd'] === 5) return bulkPrice - (price - bulkPrice) * 4;
+        if(items[Sku.IPD] < 5) return price;
+        else if(items[Sku.IPD] === 5) return bulkPrice - (price - bulkPrice) * 4;
         else return bulkPrice;
+    },
+    [Sku.VGA]: function(items, price){
+        return items[Sku.MBP] && items[Sku.MBP] >= items[Sku.VGA]? 0 : price;
     },
 }
